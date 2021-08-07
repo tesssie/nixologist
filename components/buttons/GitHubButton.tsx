34f8319ee924aa871c190975d19872905222925e
Anuj More <anujmorex@gmail.com>
2021-08-07 22:57:04 +0800

    [minor] Fix tabbing on GitHub button and allow opening of page through keyboard

--- components/buttons/GitHubButton.tsx
+++ components/buttons/GitHubButton.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
 
-import { Link } from '@chakra-ui/layout'
 import { IconButton } from '@chakra-ui/button'
 
 import { AiFillGithub } from 'react-icons/ai'
 
 export const GitHubButton = (props) => (
-  <Link href="https://github.com/execat/nixologist">
-    <IconButton
-      size="md"
-      fontSize="lg"
-      variant="ghost"
-      color="current"
-      marginLeft="2"
-      icon={<AiFillGithub />}
-      aria-label="Check source"
-      {...props}
-    />
-  </Link>
+  <IconButton
+    as="a"
+    size="md"
+    fontSize="lg"
+    variant="ghost"
+    color="current"
+    marginLeft="2"
+    icon={<AiFillGithub />}
+    aria-label="Check source"
+    href="https://github.com/execat/nixologist"
+    {...props}
+  />
 )
